169d20b29a07017052ce1df4c1b94d6eb157449e
AKASH RAI <akashrai4546@gmail.com>
2024-07-08 10:45:40 +0530

    feat: Task 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import './App.css'
 import {useState} from "react";
+import Task from "./tasks/Task.jsx";
 
 function App() {
   const [age, setAge] = useState(18)
   return (
-    <>
-      <h1 style={{color: "rebeccapurple", background: "transparent"}}>HELLO WORLD</h1>
-      <label htmlFor="age">Age: </label>
-      <input id="age" type='number' defaultValue={age} onChange={(e) => setAge(Number(e.target.value))}/>
-      <br />
-      <h3>{age < 18 ? 'Under Age' : 'Adult'} </h3>
-    </>
+    // <>
+    //   <h1 style={{color: "rebeccapurple", background: "transparent"}}>HELLO WORLD</h1>
+    //   <label htmlFor="age">Age: </label>
+    //   <input id="age" type='number' defaultValue={age} onChange={(e) => setAge(Number(e.target.value))}/>
+    //   <br />
+    //   <h3>{age < 18 ? 'Under Age' : 'Adult'} </h3>
+    // </>
+    <Task />
   )
 }
 
---
+++ src/tasks/Task.jsx
@@ -0,0 +1,19 @@
+/*
+  h1, p, list of 6 netflix series using ordered list
+ */
+import React from 'react'
+
+const series = ['Hit & Run', 'Barbarians', 'Treason', 'Marco Polo', 'Shadowhunters', 'The Recruit']
+function Task() {
+  return (
+    <>
+      <h1>Netflix</h1>
+      <p>Action Series: </p>
+      <ol>
+        {series.map((name) => <li key={name}>{name}</li>)}
+      </ol>
+    </>
+  )
+}
+
+export default Task
