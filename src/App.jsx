9e980f7123ecace95ff6449865463fba1d228e5e
AKASH RAI <akashrai4546@gmail.com>
2024-07-10 11:49:20 +0530

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import './App.css'
 import Task2 from "./tasks/Task2.jsx";
+import Task1 from "./tasks/Task1.jsx";
 
 function App() {
   // const [age, setAge] = useState(18)
-  return (
-    // <>
-    //   <h1 style={{color: "rebeccapurple", background: "transparent"}}>HELLO WORLD</h1>
-    //   <label htmlFor="age">Age: </label>
-    //   <input id="age" type='number' defaultValue={age} onChange={(e) => setAge(Number(e.target.value))}/>
-    //   <br />
-    //   <h3>{age < 18 ? 'Under Age' : 'Adult'} </h3>
-    // </>
+  // return (
+  //   <>
+  //     <h1 style={{color: "rebeccapurple", background: "transparent"}}>HELLO WORLD</h1>
+  //     <label htmlFor="age">Age: </label>
+  //     <input id="age" type='number' defaultValue={age} onChange={(e) => setAge(Number(e.target.value))}/>
+  //     <br />
+  //     <h3>{age < 18 ? 'Under Age' : 'Adult'} </h3>
+  //   </>
+  // )
 
-    // <Task1 />
+  // return <Task1 />
 
-    <Task2/>
-  )
+  return <Task2/>
 }
 
 export default App
